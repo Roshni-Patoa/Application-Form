ceed3742448d5b75974f1e9011c9e4a246a2fcc2
Roshni-Patoa <68479418+Roshni-Patoa@users.noreply.github.com>
2020-11-25 17:18:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function checkFieldsAndSubmit(){
+
+          let email = document.forms["form1"]["email"].value;
+
+          let phone = document.forms["form1"]["phone"].value;
+
+          let occupation = document.forms["form1"]["occupation"].value;
+
+          if(email !== "" && phone !== "" && occupation !== ""){
+
+            alert("Successfully Applied");
+
+            return;
+
+          }
+
+          alert("Please fill out all the required fields!")
+
+        }
+
